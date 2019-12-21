840957d3c81f9398005eaa8432cacc5ddb54b326
Kay Lächler <kay.laechler@gmx.ch>
2019-12-21 19:07:16 +0100

    did some basic graph analysis stuff

--- Project/functions.py
+++ Project/functions.py
@@ -1,1 +1,53 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+def compute_laplacian(adjacency, normalize: bool):
+    """ Return:
+        L (n x n ndarray): combinatorial or symmetric normalized Laplacian.
+    """
+    D = np.diag(np.ravel(np.sum(adjacency, axis = 1).flatten()))
+    L = D - adjacency
+    if normalize:
+        a = np.array(adjacency, dtype='float')
+        a[np.sum(a, axis = 1)==0,0] = np.Inf
+        Dn = np.diag(np.ravel(np.sum(a, axis = 1)) ** (-0.5))
+        L = Dn.dot(L).dot(Dn)
+    
+    return L
+
+
+def spectral_decomposition(laplacian: np.ndarray):
+    """ Return:
+        lamb (np.array): eigenvalues of the Laplacian
+        U (np.ndarray): corresponding eigenvectors.
+    """
+    lamb, U = np.linalg.eigh(laplacian)
+    return lamb, U
+
+
+def plot_power_lines(lon, lat):
+    fig = plt.gcf()
+    ax = plt.gca()
+    ax.plot(lon, lat, 'k', linewidth=0.3)
+
+
+def plot_signal_on_graph(lon, lat, x=None, title='', vlim=[-0.03, 0.03]):
+    fig = plt.gcf()
+    ax = plt.gca()
+    if x is not None:
+        x = np.ravel(x)
+    p = ax.scatter(lon, lat, c=x, marker='o',
+                   s=7, cmap='RdBu_r', vmin=vlim[0], vmax=vlim[1])
+    ax.dist = 7
+    ax.set_axis_off()
+    ax.set_title(title)
+    if x is not None:
+        fig.colorbar(p)
+        
+def GFT(U, signal):
+    return U.T.dot(signal)
+    
+def iGFT(U, fourier_coefficients: np.ndarray):
+    return U.dot(fourier_coefficients)
--- Project/project.py
+++ Project/project.py
@@ -2,12 +2,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from functions import *
 
 # %%
-edges = pd.read_csv('edges.csv')
-nodes = pd.read_csv('nodes.csv')
-generator = pd.read_csv('generator.csv')
-load_signal = pd.read_csv('load.csv')
+BASE_PATH = 'data/'  # adjust if necessary
+
+edges = pd.read_csv(BASE_PATH + 'edges.csv')
+nodes = pd.read_csv(BASE_PATH + 'nodes.csv')
+generator = pd.read_csv(BASE_PATH + 'generator.csv')
+load_signal = pd.read_csv(BASE_PATH + 'load.csv')
 # %%
 generator.columns
 
@@ -18,21 +21,113 @@ nodes.columns
 load_signal.columns
 
 load_signal['1']
-# %%
+# %% generate graph
 import networkx as nx
 G=nx.Graph()
 
 # to import nodes to G one needs a list of IDs and to import edges one need a list of tupples
 node_list=nodes['ID'].tolist()
-edge_list_start=edges['fromNode'].tolist()
-edge_list_end=edges['toNode'].tolist()
-edge_list=list(zip(edge_list_start,edge_list_end))
+edge_list_start_ID=edges['fromNode'].tolist()
+edge_list_end_ID=edges['toNode'].tolist()
+edge_list=list(zip(edge_list_start_ID,edge_list_end_ID))
+edge_weights=1/edges['length']  # larger lenght = smaller weight
+weighted_edge_list=list(zip(edge_list_start_ID,edge_list_end_ID,edge_weights))
+
+# lon = x, lat = y - for plotting graph on map
+lon = nodes['longitude']
+lat = nodes['latitude']
+
+edge_list_lon = np.zeros((2, len(edge_list_start_ID)))
+edge_list_lat = np.zeros((2, len(edge_list_start_ID)))
+for i in range(len(edge_list_start_ID)):
+    edge_list_lon[0, i] = nodes[nodes['ID'] == edges.iloc[i]['fromNode']]['longitude'].to_numpy()
+    edge_list_lon[1, i] = nodes[nodes['ID'] == edges.iloc[i]['toNode']]['longitude'].to_numpy()
+    edge_list_lat[0, i] = nodes[nodes['ID'] == edges.iloc[i]['fromNode']]['latitude'].to_numpy()
+    edge_list_lat[1, i] = nodes[nodes['ID'] == edges.iloc[i]['toNode']]['latitude'].to_numpy()
+    
 
 G.add_nodes_from(node_list)
-G.add_edges_from(edge_list)
+G.add_edges_from(edge_list)  # constructing unweighted graph
+#G.add_weighted_edges_from(weighted_edge_list)  # constructing weighted graph according to length of edges (lines)
 
-nx.draw(G)
+# %% plot graph
+plt.figure()
+plot_signal_on_graph(lon, lat, title='Graph representation')
+plot_power_lines(edge_list_lon, edge_list_lat)
 plt.show()
 
-#number of connected components
-nx.number_connected_components(G)
+# 3. Exploration --------------------------------------------------------------
+# %% connected components
+conn_comp = nx.number_connected_components(G)
+print(f'Number of connected components: {conn_comp}')
+# %% sparsity
+adjacency = nx.to_numpy_matrix(G)  # 'to_numpy_matrix' returns the adjacency matrix of G as a numpy matrix
+#adjacency /= np.max(adjacency)  # if weighted graph
+plt.figure
+plt.spy(adjacency, markersize=0.5)
+plt.show()
+# %% diameter
+diam = nx.diameter(G)
+print(f'Diameter: {diam}')
+# %% degree distribution (looks like power law)
+degrees = np.count_nonzero(adjacency, axis=1)
+plt.figure
+plt.hist(degrees)
+plt.title('Degree distribution')
+plt.show()
+# %% spectrum
+# computing normalized Laplacian
+laplacian = compute_laplacian(adjacency, normalize=True)  # adjacency matrix need to be unweighted!
+# spectral decomposition
+lam, U = spectral_decomposition(laplacian)
+
+plotfig = True  # set false to not plot eigenvector visualisation
+if plotfig is True:
+    plt.figure(figsize=(18, 9))
+    plt.subplot(231)
+    plot_signal_on_graph(lon, lat, x=U[:,0], title='Eigenvector #0')
+    plot_power_lines(edge_list_lon, edge_list_lat)
+    plt.subplot(232)
+    plot_signal_on_graph(lon, lat, x=U[:,1], title='Eigenvector #1')
+    plot_power_lines(edge_list_lon, edge_list_lat)
+    plt.subplot(233)
+    plot_signal_on_graph(lon, lat, x=U[:,2], title='Eigenvector #2')
+    plot_power_lines(edge_list_lon, edge_list_lat)
+    plt.subplot(234)
+    plot_signal_on_graph(lon, lat, x=U[:,3], title='Eigenvector #3')
+    plot_power_lines(edge_list_lon, edge_list_lat)
+    plt.subplot(235)
+    plot_signal_on_graph(lon, lat, x=U[:,10], title='Eigenvector #10')
+    plot_power_lines(edge_list_lon, edge_list_lat)
+    plt.subplot(236)
+    plot_signal_on_graph(lon, lat, x=U[:,100], title='Eigenvector #100')
+    plot_power_lines(edge_list_lon, edge_list_lat)
+
+# %% define and plot load signal
+# extract load signal from start_hour to end_hour
+start_hour = 0
+end_hour = 23
+load_day1 = load_signal.iloc[start_hour:end_hour + 1].to_numpy()[:, 1:]
+# average the load signals
+mean_load_day1 = np.mean(load_day1, axis=0)
+mean_load_day1 /= np.max(mean_load_day1)
+mean_load_day1 = mean_load_day1.reshape(len(mean_load_day1), -1)
+# plot averaged load signal on graph (adjust vlim if necessary, vlim are the clip values of the signal for representation)
+plt.figure()
+plot_signal_on_graph(lon, lat, x=mean_load_day1, title='Normalised load over one day', vlim=(0, 0.06))
+plot_power_lines(edge_list_lon, edge_list_lat)
+plt.show()
+                         
+# %% plotting spectrum of the average load signal
+plt.figure
+plt.plot(lam, np.abs(GFT(U, mean_load_day1)))
+plt.xlabel('$\lambda$')
+plt.ylabel('GFT')
+plt.title('Spectrum of the average load signal')
+plt.show()
+# %% clustering coefficient
+clustering_coeff = nx.average_clustering(G)
+print(f'Clustering coefficient: {clustering_coeff}')
+
+
+
