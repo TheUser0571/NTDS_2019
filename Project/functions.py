8dc10012b2848de2003c5edee96cec66746e46fc
Viktor Crettenand <viktor.crettenand@gmail.com>
2019-12-25 20:19:05 +0100

    Fourier transform of time series

--- Project/functions.py
+++ Project/functions.py
@@ -3,19 +3,26 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
+# -----------------------------------------------------------------------------------------------------------------
+
+
 def compute_laplacian(adjacency, normalize: bool):
-    """ Return:
-        L (n x n ndarray): combinatorial or symmetric normalized Laplacian.
     """
-    D = np.diag(np.ravel(np.sum(adjacency, axis = 1).flatten()))
+    Return:
+    L (n x n ndarray): combinatorial or symmetric normalized Laplacian.
+    """
+    D = np.diag(np.ravel(np.sum(adjacency, axis=1).flatten()))
     L = D - adjacency
     if normalize:
         a = np.array(adjacency, dtype='float')
-        a[np.sum(a, axis = 1)==0,0] = np.Inf
-        Dn = np.diag(np.ravel(np.sum(a, axis = 1)) ** (-0.5))
+        a[np.sum(a, axis=1) == 0, 0] = np.Inf
+        Dn = np.diag(np.ravel(np.sum(a, axis=1)) ** (-0.5))
         L = Dn.dot(L).dot(Dn)
-    
+
     return L
+
+
+# -----------------------------------------------------------------------------------------------------------------
 
 
 def spectral_decomposition(laplacian: np.ndarray):
@@ -27,13 +34,45 @@ def spectral_decomposition(laplacian: np.ndarray):
     return lamb, U
 
 
+# -----------------------------------------------------------------------------------------------------------------
+
+
 def plot_power_lines(lon, lat):
+    """
+    Plots the edges (=power lines) between the nodes.
+
+    Plots a line for each edge.
+
+    Parameters:
+    lon (numpy.ndarray): longitude vector of shape (2, number of edges) containing the longitude coordinate of the starting nodes in the first line and of the ending nodes in the second line
+    lat (numpy.ndarray): latitude vector of shape (2, number of edges)
+
+    """
     fig = plt.gcf()
     ax = plt.gca()
     ax.plot(lon, lat, 'k', linewidth=0.3)
 
 
+# -----------------------------------------------------------------------------------------------------------------
+
+
 def plot_signal_on_graph(lon, lat, x=None, title='', vlim=[-0.03, 0.03]):
+    """
+   Plots signal on graph
+
+   You have to create a figure before calling this function.
+
+   Parameters:
+   lon (pandas.core.series.Series) : longitude
+   lat (pandas.core.series.Series) : latitude
+   x :
+   title :
+   vlim :
+
+   Returns:
+   no returns
+
+    """
     fig = plt.gcf()
     ax = plt.gca()
     if x is not None:
@@ -45,9 +84,60 @@ def plot_signal_on_graph(lon, lat, x=None, title='', vlim=[-0.03, 0.03]):
     ax.set_title(title)
     if x is not None:
         fig.colorbar(p)
-        
+
+
+# -----------------------------------------------------------------------------------------------------------------
+
+
 def GFT(U, signal):
+    """
+    Fourier transform
+
+    U : the discrete Fourier transform is a linear transformation. U is the matrix of that transformation
+    signal : signal to be transformed to the frequency domain
+    """
     return U.T.dot(signal)
-    
+
+
+# -----------------------------------------------------------------------------------------------------------------
+
+
 def iGFT(U, fourier_coefficients: np.ndarray):
+    """
+    Inverse Fourier transform
+
+    Parameters:
+    U : inverse of the matrix for discrete Fourier transform
+    fourier_coefficients : signal to be transformed from the frequency domain to the time domain
+
+    """
     return U.dot(fourier_coefficients)
+
+
+# -----------------------------------------------------------------------------------------------------------------
+
+def magnitude_getter(signal):
+    """
+    Gets the components of the Fourier decomposition corresponding to the frequency of 1 day, 1 week and 1 year.
+    """
+    # n_sample = len(signal)
+    # sampling_frequency = 1  # because we sample once an hour -> 24 times a day
+    signal_hat = np.fft.fft(signal)
+    sum=np.sum(np.abs(signal_hat))
+    signal_hat = signal_hat/sum # normalization to be able to compare different nodes
+    # f = np.arange(0, len(signal_hat) // 2, 1) * sampling_frequency / n_sample  # frequencies for the x axis
+    # temp_day= np.abs(f - 1 / 24)
+    # temp_week= np.abs(f- 1/(7*24))
+    # temp_year= np.abs(f - 1 / (365 * 24))
+    # index_day=np.argmin(temp_day)
+    # index_week=np.argmin(temp_week)
+    # index_year=np.argmin(temp_year)
+    # print(index_year, index_week,index_day)
+    # day_mag = np.abs(signal_hat[index_day-3:index_day+4])
+    # week_mag = np.abs(signal_hat[index_week-3:index_week+4])
+    # year_mag = np.abs(signal_hat[index_year-3:index_year+4])
+    half_day_mag=np.abs(signal_hat[2192])
+    day_mag = np.abs(signal_hat[1096])
+    week_mag = np.abs(signal_hat[157])
+    year_mag = np.abs(signal_hat[3])
+    return half_day_mag, day_mag, week_mag, year_mag
--- Project/project.py
+++ Project/project.py
@@ -2,10 +2,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+import functions
 from functions import *
+import networkx as nx
 
 # %%
-BASE_PATH = 'data/'  # adjust if necessary
+BASE_PATH = 'P:/Python/Network_tour/Data_for_project/'  # adjust if necessary
 
 edges = pd.read_csv(BASE_PATH + 'edges.csv')
 nodes = pd.read_csv(BASE_PATH + 'nodes.csv')
@@ -22,16 +25,17 @@ load_signal.columns
 
 load_signal['1']
 # %% generate graph
-import networkx as nx
-G=nx.Graph()
 
-# to import nodes to G one needs a list of IDs and to import edges one need a list of tupples
-node_list=nodes['ID'].tolist()
-edge_list_start_ID=edges['fromNode'].tolist()
-edge_list_end_ID=edges['toNode'].tolist()
-edge_list=list(zip(edge_list_start_ID,edge_list_end_ID))
-edge_weights=1/edges['length']  # larger lenght = smaller weight
-weighted_edge_list=list(zip(edge_list_start_ID,edge_list_end_ID,edge_weights))
+
+graph = nx.Graph()
+
+# to import nodes to graph one needs a list of IDs and to import edges one need a list of tuples
+node_list = nodes['ID'].tolist()
+edge_list_start_ID = edges['fromNode'].tolist()
+edge_list_end_ID = edges['toNode'].tolist()
+edge_list = list(zip(edge_list_start_ID, edge_list_end_ID))
+edge_weights = 1 / edges['length']  # larger length = smaller weight
+weighted_edge_list = list(zip(edge_list_start_ID, edge_list_end_ID, edge_weights))
 
 # lon = x, lat = y - for plotting graph on map
 lon = nodes['longitude']
@@ -44,11 +48,10 @@ for i in range(len(edge_list_start_ID)):
     edge_list_lon[1, i] = nodes[nodes['ID'] == edges.iloc[i]['toNode']]['longitude'].to_numpy()
     edge_list_lat[0, i] = nodes[nodes['ID'] == edges.iloc[i]['fromNode']]['latitude'].to_numpy()
     edge_list_lat[1, i] = nodes[nodes['ID'] == edges.iloc[i]['toNode']]['latitude'].to_numpy()
-    
 
-G.add_nodes_from(node_list)
-G.add_edges_from(edge_list)  # constructing unweighted graph
-#G.add_weighted_edges_from(weighted_edge_list)  # constructing weighted graph according to length of edges (lines)
+graph.add_nodes_from(node_list)
+# graph.add_edges_from(edge_list)  # constructing unweighted graph
+graph.add_weighted_edges_from(weighted_edge_list)  # constructing weighted graph according to length of edges (lines)
 
 # %% plot graph
 plt.figure()
@@ -58,17 +61,22 @@ plt.show()
 
 # 3. Exploration --------------------------------------------------------------
 # %% connected components
-conn_comp = nx.number_connected_components(G)
+conn_comp = nx.number_connected_components(graph)
 print(f'Number of connected components: {conn_comp}')
 # %% sparsity
-adjacency = nx.to_numpy_matrix(G)  # 'to_numpy_matrix' returns the adjacency matrix of G as a numpy matrix
-#adjacency /= np.max(adjacency)  # if weighted graph
+adjacency = nx.to_numpy_matrix(graph)  # 'to_numpy_matrix' returns the adjacency matrix of G as a numpy matrix
+adjacency /= np.max(adjacency)  # if weighted graph to normalise
 plt.figure
 plt.spy(adjacency, markersize=0.5)
 plt.show()
 # %% diameter
-diam = nx.diameter(G)
+diam = nx.diameter(graph)
 print(f'Diameter: {diam}')
+#
+# would be interesting to know what is the largest shortest path is kilometers
+#
+#
+
 # %% degree distribution (looks like power law)
 degrees = np.count_nonzero(adjacency, axis=1)
 plt.figure
@@ -77,31 +85,32 @@ plt.title('Degree distribution')
 plt.show()
 # %% spectrum
 # computing normalized Laplacian
-laplacian = compute_laplacian(adjacency, normalize=True)  # adjacency matrix need to be unweighted!
+laplacian = compute_laplacian(adjacency, normalize=True)  # adjacency matrix needs to be unweighted!
 # spectral decomposition
 lam, U = spectral_decomposition(laplacian)
 
-plotfig = True  # set false to not plot eigenvector visualisation
-if plotfig is True:
+plot_fig = True  # set false to not plot eigenfunctions
+if plot_fig is True:
     plt.figure(figsize=(18, 9))
     plt.subplot(231)
-    plot_signal_on_graph(lon, lat, x=U[:,0], title='Eigenvector #0')
+    plot_signal_on_graph(lon, lat, x=U[:, 0], title='Eigenfunction #0 $\lambda$ =' + str(float('%.1g' % lam[0])))
     plot_power_lines(edge_list_lon, edge_list_lat)
     plt.subplot(232)
-    plot_signal_on_graph(lon, lat, x=U[:,1], title='Eigenvector #1')
+    plot_signal_on_graph(lon, lat, x=U[:, 1], title='Eigenfunction #1 $\lambda$ =' + str(float('%.1g' % lam[1])))
     plot_power_lines(edge_list_lon, edge_list_lat)
     plt.subplot(233)
-    plot_signal_on_graph(lon, lat, x=U[:,2], title='Eigenvector #2')
+    plot_signal_on_graph(lon, lat, x=U[:, 2], title='Eigenfunction #2 $\lambda$ =' + str(float('%.1g' % lam[2])))
     plot_power_lines(edge_list_lon, edge_list_lat)
     plt.subplot(234)
-    plot_signal_on_graph(lon, lat, x=U[:,3], title='Eigenvector #3')
+    plot_signal_on_graph(lon, lat, x=U[:, 3], title='Eigenfunction #3 $\lambda$ =' + str(float('%.1g' % lam[3])))
     plot_power_lines(edge_list_lon, edge_list_lat)
     plt.subplot(235)
-    plot_signal_on_graph(lon, lat, x=U[:,10], title='Eigenvector #10')
+    plot_signal_on_graph(lon, lat, x=U[:, 10], title='Eigenfunction #10 $\lambda$ =' + str(float('%.1g' % lam[10])))
     plot_power_lines(edge_list_lon, edge_list_lat)
     plt.subplot(236)
-    plot_signal_on_graph(lon, lat, x=U[:,100], title='Eigenvector #100')
+    plot_signal_on_graph(lon, lat, x=U[:, 100], title='Eigenfunction #100 $\lambda$ =' + str(float('%.1g' % lam[100])))
     plot_power_lines(edge_list_lon, edge_list_lat)
+    plt.show()
 
 # %% define and plot load signal
 # extract load signal from start_hour to end_hour
@@ -112,12 +121,13 @@ load_day1 = load_signal.iloc[start_hour:end_hour + 1].to_numpy()[:, 1:]
 mean_load_day1 = np.mean(load_day1, axis=0)
 mean_load_day1 /= np.max(mean_load_day1)
 mean_load_day1 = mean_load_day1.reshape(len(mean_load_day1), -1)
+
 # plot averaged load signal on graph (adjust vlim if necessary, vlim are the clip values of the signal for representation)
 plt.figure()
 plot_signal_on_graph(lon, lat, x=mean_load_day1, title='Normalised load over one day', vlim=(0, 0.06))
 plot_power_lines(edge_list_lon, edge_list_lat)
 plt.show()
-                         
+
 # %% plotting spectrum of the average load signal
 plt.figure
 plt.plot(lam, np.abs(GFT(U, mean_load_day1)))
@@ -126,8 +136,64 @@ plt.ylabel('GFT')
 plt.title('Spectrum of the average load signal')
 plt.show()
 # %% clustering coefficient
-clustering_coeff = nx.average_clustering(G)
+clustering_coeff = nx.average_clustering(graph)
 print(f'Clustering coefficient: {clustering_coeff}')
 
+# %% fourier transform of time series
+load_node = load_signal.to_numpy()[:, 4]
+n_sample=len(load_node)
+x = np.arange(n_sample)
+sampling_frequency = 1 # because we sample once an hour -> 24 times a day
+load_node_hat = np.fft.fft(load_node)
+f = np.arange(0, len(load_node_hat) // 2, 1) * sampling_frequency / n_sample # frequencies for the x axis
+plt.figure(1)
+ax = plt.gca()
+ax.plot(f, np.abs(load_node_hat)[:len(load_node) // 2], c='red')
+ax.scatter([1/(365*24), 1/(7*24), 1/24,1/12], [100000, 100000, 100000,100000], c='green', marker='o') # plotting green points at frequencies of 1 day, 1 week and 1 year arbitrary height
+plt.show()
+print(np.sum(np.abs(load_node_hat)))
 
+# %%
+# creat signals on graph called day, week, year which are the frequency magnitudes of the Fourier decomposition
+import importlib
+import functions
+importlib.reload(functions)
+
+numpy_load_signal=load_signal.to_numpy()[:,1:]
+half_day=np.zeros((numpy_load_signal.shape[1],1))
+day=np.zeros((numpy_load_signal.shape[1],1))
+week=np.zeros((numpy_load_signal.shape[1],1))
+year=np.zeros((numpy_load_signal.shape[1],1))
+for i in range(numpy_load_signal.shape[1]):
+    load_node = numpy_load_signal[:, i]
+    temp_half_day, temp_day, temp_week, temp_year=functions.magnitude_getter(load_node)
+    half_day[i]=temp_half_day
+    day[i]=temp_day
+    week[i]=temp_week
+    year[i]=temp_year
+
+half_day[(np.isnan(half_day))]=0
+day[(np.isnan(day))]=0
+week[(np.isnan(week))]=0
+year[(np.isnan(year))]=0
+# %%
+# plotting day, week and year signals on graph
+
+plt.figure(figsize=(18, 9))
+plt.subplot(221)
+plot_signal_on_graph(lon, lat, x=half_day, title='Fourier component of load time series corresponding to once every half day frequency', vlim=(0, np.max(half_day)))
+plot_power_lines(edge_list_lon, edge_list_lat)
+
+plt.subplot(222)
+plot_signal_on_graph(lon, lat, x=day, title='Fourier component of load time series corresponding to once a day frequency', vlim=(0, np.max(day)))
+plot_power_lines(edge_list_lon, edge_list_lat)
+
+plt.subplot(223)
+plot_signal_on_graph(lon, lat, x=week, title='Fourier component of load time series corresponding to once a week frequency', vlim=(0, np.max(week)))
+plot_power_lines(edge_list_lon, edge_list_lat)
+
+plt.subplot(224)
+plot_signal_on_graph(lon, lat, x=year, title='Fourier component of load time series corresponding to once a year frequency', vlim=(0, np.max(year)))
+plot_power_lines(edge_list_lon, edge_list_lat)
+plt.show()
 
